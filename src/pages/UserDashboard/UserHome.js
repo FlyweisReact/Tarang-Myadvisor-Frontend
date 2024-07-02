d955ab1c23021f6147abfd52d9e247293d4058dc
React <114087216+FlyweisReact@users.noreply.github.com>
2024-07-02 19:14:22 +0530

    changes

--- src/pages/UserDashboard/UserHome.js
+++ src/pages/UserDashboard/UserHome.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import React from "react";
-import { Autoplay } from "swiper/modules";
 import {
   adwizorImg1,
   adwizorImg2,
@@ -10,122 +9,121 @@ import {
   adwizorImg5,
   adwizorImg6,
 } from "../../assest";
-import { Slider } from "../../components/Sliders/Sliders";
 import DashboardLayout from "../../Layout/UserDashboardLayout/DashboardLayout";
 
-const topAdvizors = [
-  {
-    img: adwizorImg1,
-    name: "Subash Mishra",
-    rating: "9.5/10",
-    description: [
-      "3+ Years (Management).",
-      "Gujrat , India.",
-      "2K+ Students , Helped",
-    ],
-  },
-  {
-    img: adwizorImg2,
-    name: "Manisha Roy",
-    rating: "9.5/10",
-    description: [
-      "5+ Years (Civils).",
-      "Gujrat , India.",
-      "2K+ Students , Helped",
-    ],
-  },
-  {
-    img: adwizorImg3,
-    name: "Nisha Agarwal",
-    rating: "9.5/10",
-    description: [
-      "3+ Years (Management).",
-      "Gujrat , India.",
-      "2K+ Students , Helped",
-    ],
-  },
-  {
-    img: adwizorImg4,
-    name: "Subash Mishra",
-    rating: "9.5/10",
-    description: [
-      "3+ Years (Management).",
-      "Gujrat , India.",
-      "2K+ Students , Helped",
-    ],
-  },
-  {
-    img: adwizorImg5,
-    name: "Manisha Roy",
-    rating: "9.5/10",
-    description: [
-      "3+ Years (Management).",
-      "Gujrat , India.",
-      "2K+ Students , Helped",
-    ],
-  },
-  {
-    img: adwizorImg6,
-    name: "Nisha Agarwal",
-    rating: "9.5/10",
-    description: [
-      "3+ Years (Management).",
-      "Gujrat , India.",
-      "2K+ Students , Helped",
-    ],
-  },
-];
+// const topAdvizors = [
+//   {
+//     img: adwizorImg1,
+//     name: "Subash Mishra",
+//     rating: "9.5/10",
+//     description: [
+//       "3+ Years (Management).",
+//       "Gujrat , India.",
+//       "2K+ Students , Helped",
+//     ],
+//   },
+//   {
+//     img: adwizorImg2,
+//     name: "Manisha Roy",
+//     rating: "9.5/10",
+//     description: [
+//       "5+ Years (Civils).",
+//       "Gujrat , India.",
+//       "2K+ Students , Helped",
+//     ],
+//   },
+//   {
+//     img: adwizorImg3,
+//     name: "Nisha Agarwal",
+//     rating: "9.5/10",
+//     description: [
+//       "3+ Years (Management).",
+//       "Gujrat , India.",
+//       "2K+ Students , Helped",
+//     ],
+//   },
+//   {
+//     img: adwizorImg4,
+//     name: "Subash Mishra",
+//     rating: "9.5/10",
+//     description: [
+//       "3+ Years (Management).",
+//       "Gujrat , India.",
+//       "2K+ Students , Helped",
+//     ],
+//   },
+//   {
+//     img: adwizorImg5,
+//     name: "Manisha Roy",
+//     rating: "9.5/10",
+//     description: [
+//       "3+ Years (Management).",
+//       "Gujrat , India.",
+//       "2K+ Students , Helped",
+//     ],
+//   },
+//   {
+//     img: adwizorImg6,
+//     name: "Nisha Agarwal",
+//     rating: "9.5/10",
+//     description: [
+//       "3+ Years (Management).",
+//       "Gujrat , India.",
+//       "2K+ Students , Helped",
+//     ],
+//   },
+// ];
 
-const renderAdwizorCards = (item) => {
-  return (
-    <div className="card">
-      <div className="detail">
-        <img src={item.img} alt="" className="mainImg" />
-        <div className="content">
-          <p className="title"> {item.name} </p>
-          <div className="rating">
-            <i className="fa-solid fa-star"></i>
-            <p>{item.rating} </p>
-          </div>
-          {item.description.map((i, index) => (
-            <p className="faded" key={`desc${index}`}>
-              {" "}
-              {i}{" "}
-            </p>
-          ))}
-        </div>
-      </div>
-      <div className="btn-container">
-        <button>Book Appointment</button>
-        <button>View Profile</button>
-      </div>
-    </div>
-  );
-};
+// const renderAdwizorCards = (item) => {
+//   return (
+//     <div className="card">
+//       <div className="detail">
+//         <img src={item.img} alt="" className="mainImg" />
+//         <div className="content">
+//           <p className="title"> {item.name} </p>
+//           <div className="rating">
+//             <i className="fa-solid fa-star"></i>
+//             <p>{item.rating} </p>
+//           </div>
+//           {item.description.map((i, index) => (
+//             <p className="faded" key={`desc${index}`}>
+//               {" "}
+//               {i}{" "}
+//             </p>
+//           ))}
+//         </div>
+//       </div>
+//       <div className="btn-container">
+//         <button>Book Appointment</button>
+//         <button>View Profile</button>
+//       </div>
+//     </div>
+//   );
+// };
 
-const topAdvizorsConfig = {
-  spaceBetween: 20,
-  loop: true,
-  slidesPerView: 1,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: false,
-  },
-  speed: 4000,
-  resistance: false,
-  resistanceRatio: 0,
-  breakpoints: {
-    768: {
-      slidesPerView: 1,
-    },
-    900: {
-      slidesPerView: 3.5,
-    },
-    1024: {
-      slidesPerView: 3.5,
-    },
-  },
-};
+// const topAdvizorsConfig = {
+//   spaceBetween: 20,
+//   loop: true,
+//   slidesPerView: 1,
+//   autoplay: {
+//     delay: 0,
+//     disableOnInteraction: false,
+//   },
+//   speed: 4000,
+//   resistance: false,
+//   resistanceRatio: 0,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 1,
+//     },
+//     900: {
+//       slidesPerView: 3.5,
+//     },
+//     1024: {
+//       slidesPerView: 3.5,
+//     },
+//   },
+// };
 
 const UserHome = () => {
   return (
